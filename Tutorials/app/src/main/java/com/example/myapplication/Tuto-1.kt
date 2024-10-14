c3ed6ecaba5c4fa8c770530b757523d93942c237
Samiaboug <bougtoubsamia@gmail.com>
2024-10-14 09:39:52 +0100

    tutorials

---
+++ Tutorials/app/src/main/java/com/example/myapplication/Tuto-1.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+/* fun main(){
+    val traficLihtColor = "Green"
+    if(traficLihtColor == "Red"){
+        println("Stop")
+    }else{
+        println("Go")
+    }
+
+} */
+/*fun main(){
+    //
+    val traficLightColor = "Gren"
+    when(traficLightColor){
+        "Red" -> println("Stop")
+        "Green" -> println("Go")
+        else -> println("Invalid light color")
+    }
+
+    //
+
+
+}*/
+fun main(){
+
+        val trafficLightColor = "Black"
+
+      var message =  when (trafficLightColor ) {
+          "Red" -> "Stop"
+          "Yellow" -> "Slow"
+          "Green" -> "Go"
+          else ->
+              "Invalid traffic-light color"
+
+      }
+
+    println(message)
+}
