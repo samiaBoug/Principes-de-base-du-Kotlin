cc1f8c847366496b90c09c091cd26ac7a5c40b90
Samiaboug <bougtoubsamia@gmail.com>
2024-10-14 12:21:42 +0100

    Projet

--- Tutorials/app/src/main/java/com/example/myapplication/LiveCoding.kt
+++ Tutorials/app/src/main/java/com/example/myapplication/LiveCoding.kt
@@ -1,2 +1,18 @@
 package com.example.myapplication
 
+fun main(){
+  /*val name = "Samia"
+    if(name == "Samia") {
+        println("True")
+    }else {
+        println("False")
+    } */
+    val traficLightColor = "Green"
+
+    val message = when(traficLightColor){
+        "Red" -> "Stop"
+        "Green" -> "Go"
+        else -> "Invalid color"
+    }
+    println(message)
+    }
--- Tutorials/app/src/main/java/com/example/myapplication/tuto-1-3.kt
+++ Tutorials/app/src/main/java/com/example/myapplication/tuto-1-3.kt
@@ -1,4 +1,23 @@
 package com.example.myapplication
 
-class tuto-1-3 {
+class SmartDevice(val name: String , val category: String ){
+
+   /* val name = "Android TV"
+    val category = "Entertainment" */
+    var deviceStatus = "online"
+
+    fun turnOn(){
+        println("Smart device is turned on.")
+    }
+    fun turnOff(){
+        println("Smart device is turned off.")
+    }
+}
+fun main(){
+
+    val smartTvDevice = SmartDevice("Android TV" , "Entertainment")
+    smartTvDevice.turnOff()
+    smartTvDevice.turnOn()
+   // println("name of device : "+ smartTvDevice.name)
+
 }
