46a348711903bfc21a0c46d2a9c29c124f174fd4
Samiaboug <bougtoubsamia@gmail.com>
2024-10-15 11:58:15 +0100

    tuto 1-4

--- Tutorials/app/src/main/java/com/example/myapplication/Tuto-1-4.kt
+++ Tutorials/app/src/main/java/com/example/myapplication/Tuto-1-4.kt
@@ -1,12 +1,55 @@
 package com.example.myapplication
 
 fun main (){
-    val trickFunction = trick
+   /* val trickFunction = trick
     trick()
     trickFunction()
+    treat()
+
+    */
+    /* val coins: (Int) -> String = {
+        "$it quarters"
+    } */
+
+    /*val cupcake: (Int) -> String = {
+        "Have a cupcake!"
+    }*/
+
+    val treatFunction = trickOrTreat(false) { "$it quarter" }
+    val trickFunction = trickOrTreat(true, null)
+    treatFunction()
+
+    repeat(5){
+        trickFunction()
+
+    }
 
 
 }
  val trick = {
     println("no treats!")
 }
+val  treat: () -> Unit = {
+    println("have a treat")
+}
+/*fun treatOrTrick(isTrick: Boolean): ()->Unit {
+    return if(isTrick){
+        trick
+    }else {
+        treat
+    }
+
+
+}
+*/
+fun trickOrTreat(isTrick: Boolean , extraTreat: ((Int)-> String)?) : ()-> Unit{
+     if(isTrick){
+      return  trick
+    }else{
+        if(extraTreat != null) {
+            print(extraTreat(5))
+        }
+       return treat
+    }
+}
+
